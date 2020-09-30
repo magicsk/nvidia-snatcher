db5579b4556474e74511418851fea49c6692b00f
Danilo Heroso de Deus Pereira <danilo.heroso@serpro.gov.br>
2020-09-30 16:45:36 -0300

    Updating Terabyte store cards, while adding new brands.

--- src/store/model/terabyte.ts
+++ src/store/model/terabyte.ts
@@ -31,7 +31,188 @@ export const Terabyte: Store = {
 			model: 'tuf-oc',
 			series: '3080',
 			url: 'https://www.terabyteshop.com.br/produto/15106/placa-de-video-asus-tuf-geforce-rtx-3080-10gb-gddr6x-320bit-tuf-rtx3080-o10g-gaming'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin-x2-oc',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15124/placa-de-video-inno3d-geforce-twin-x2-oc-rtx-3080-10gb-gddr6x-320bit-n30802-106xx-1810va34'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill-x3',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15120/placa-de-video-inno3d-geforce-ichill-x3-rtx-3080-10gb-gddr6x-320bit-inno3d-geforce-rtx-3080-ichill-x3'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill-x4',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15122/placa-de-video-inno3d-geforce-ichill-x4-rtx-3080-10gb-gddr6x-320bit-c30804-106xx-1810va36'
+		},
+		{
+			brand: 'galax',
+			model: 'sg-oc',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15123/placa-de-video-galax-geforce-rtx-3080-sg-1-click-oc-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15127/placa-de-video-galax-geforce-rtx-3080-ex-gaming-1-click-oc-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc-pink',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15129/placa-de-video-galax-geforce-rtx-3080-ex-gaming-pink-1-click-oc-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc-white',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15130/placa-de-video-galax-geforce-rtx-3080-ex-gaming-white-1-click-oc-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'colorful',
+			model: 'igame-advanced-10g',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15144/placa-de-video-colorful-geforce-igame-rtx-3080-advanced-10g-v-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming-x-trio',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15145/placa-de-video-msi-geforce-rtx-3080-gaming-x-trio-10gb-gddr6x-320bit-geforce-rtx-3080-gaming-x-trio'
+		},
+		{
+			brand: 'colorful',
+			model: 'igame-ultra-10g',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15147/placa-de-video-colorful-geforce-igame-rtx-3080-ultra-10g-v-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus-3x-oc',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15148/placa-de-video-msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-320bit-geforce-rtx-3080-ventus-3x-oc'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus-3x',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15149/placa-de-video-msi-geforce-rtx-3080-ventus-3x-10gb-gddr6x-320bit-geforce-rtx-3080-ventus'
+		},
+		{
+			brand: 'colorful',
+			model: 'igame-vulcan-10g',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15150/placa-de-video-colorful-geforce-igame-rtx-3080-vulcan-10g-v-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'colorful',
+			model: 'igame-neptune-10g',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15152/placa-de-video-colorful-geforce-igame-rtx-3080-neptune-10g-v-10gb-gddr6x-320bit'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8-gaming-rgb-xppb',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15090/placa-de-video-pny-geforce-rtx-3080-xlr8-gaming-epic-x-rgb-triple-10gb-gddr6x-320bit-vcg308010tfxppb'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8-gaming-rgb-xmpb',
+			series: '3080',
+			url: 'https://www.terabyteshop.com.br/produto/15092/placa-de-video-pny-geforce-rtx-3080-xlr8-gaming-epic-x-rgb-triple-10gb-gddr6x-320bit-vcg308010tfxmpb'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8-gaming-epic-rgb-xppb',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15093/placa-de-video-pny-geforce-rtx-3090-xlr8-gaming-epic-x-rgb-triple-24gb-gddr6x-384bit-vcg309024tfxppb'
+		},
+		{
+			brand: 'asus',
+			model: 'rog-strix-oc',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15101/placa-de-video-asus-rog-strix-geforce-rtx-3090-24gb-gddr6x-384bit-rog-strix-rtx3090-o24g-gaming'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf-oc',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15108/placa-de-video-asus-tuf-geforce-rtx-3090-24gb-gddr6x-384bit-tuf-rtx3090-o24-gaming'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15110/placa-de-video-asus-tuf-geforce-rtx-3090-24gb-gddr6x-384bit-tuf-rtx3090-24-gaming'
+		},
+		{
+			brand: 'galax',
+			model: 'sg-oc',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15125/placa-de-video-galax-geforce-rtx-3090-sg-1-click-oc-24gb-gddr6x-384bit'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill-x4',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15126/placa-de-video-inno3d-geforce-ichill-x4-rtx-3090-24gb-gddr6x-384bit-c30904-246xx-1880va36'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15132/placa-de-video-galax-geforce-rtx-3090-ex-gaming-1-click-oc-24gb-gddr6x-384bit'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc-pink',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15133/placa-de-video-galax-geforce-rtx-3090-ex-gaming-pink-1-click-oc-24gb-gddr6x-384bit'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming-x-trio',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15139/placa-de-video-msi-geforce-rtx-3090-gaming-x-trio-24gb-gddr6x-384bit-geforce-rtx-3090-gaming-x-trio'
+		},
+		{
+			brand: 'galax',
+			model: 'ex-gaming-oc-white',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15136/placa-de-video-galax-geforce-rtx-3090-ex-gaming-white-1-click-oc-24gb-gddr6x-384bit'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus-3x',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15142/placa-de-video-msi-geforce-rtx-3090-ventus-3x-24gb-gddr6x-384bit-geforce-rtx-3090-ventus-3x'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming-trio',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15140/placa-de-video-msi-geforce-rtx-3090-gaming-trio-24gb-gddr6x-384bit-geforce-rtx-3090-gaming-trio'
+		},
+		{
+			brand: 'asus',
+			model: 'rog-strix',
+			series: '3090',
+			url: 'https://www.terabyteshop.com.br/produto/15089/placa-de-video-asus-rog-strix-geforce-rtx-3090-24gb-gddr6x-384bit-rog-strix-rtx3090-24g-gaming'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8-gaming-epic-x-rgb-xmpb',
+			series: '3090',
+			url: ' https://www.terabyteshop.com.br/produto/15091/placa-de-video-pny-geforce-rtx-3090-xlr8-gaming-epic-x-rgb-triple-24gb-gddr6x-384bit-vcg309024tfxmpb'
 		}
 	],
 	name: 'terabyte'
 };
+
